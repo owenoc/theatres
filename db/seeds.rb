9d56a9cc0e6789981086b8d220b8ab19cd662e50
Owen <o.oconnor7@nuigalway.ie>
2014-11-03 14:02:32 +0000

    columns

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# encoding: utf-8
+Place.delete_all
 
-Product.delete_all
-
-Product.create!(title: "O'Flaherty",
+Place.create!(title: "O'Flaherty",
   description: 
     %{<p>
         Big lecture theatre with 2 projectors. Used mainly for 1st year modules
       </p>},
   image_url: 'oflat.jpg',    
-  capacity: 200
-  average_attendence: 100
+  capacity: 200,
+  average_attendence: 100,
+  location: 'Concourse')
+
+Place.create!(title: "O'Malley",
+  description: 
+    %{<p>
+        Big lecture theatre with 2 projectors. Used mainly for 1st year modules
+      </p>},
+  image_url: 'oflat.jpg',    
+  capacity: 200,
+  average_attendence: 100,
   location: 'Concourse')
